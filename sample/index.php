938305effb019075048f86d2540080a21116f064
Javier Zapata <javierzapata82@gmail.com>
2016-09-27 20:54:36 +0200

    Update sample

--- sample/index.php
+++ sample/index.php
@@ -9,26 +9,31 @@ use SpanishGuestReportGenerator\HotelFactory;
 use SpanishGuestReportGenerator\GuestFactory;
 
 
+// Dummy content
+$chainCode = 'ZYXWV98765';          // Only required for multiple hotels
+$chainName = 'Simple Tiger Hotels'; // Only required for multiple hotels
+
 $reportDateTime = new \DateTime('2016-09-26 00:00:00');
-$reportNumber   = 321;
+$reportNumber   = 321; // Determines the extension of the exported file
+
 $guests = [
     [
         'isSpanish'    => true,
-        'idNumber'     => '00126939278',
-        'docType'      => 'd',
-        'docIssueDate' => '2013-02-16',
+        'idNumber'     => '00126939278', // 11 chars. max. for Spanish DNI
+        'docType'      => 'd',           // Spanish DNI#/NIF
+        'docIssueDate' => '2013-02-16',  // Y-m-d, d-m-Y, Ymd, d.m.Y, d/m/Y
         'lastName1'    => 'Eutychus',
-        'lastName2'    => 'Tarik',
+        'lastName2'    => 'Tarik',       // Required for Spanish citizens
         'firstName'    => 'Orlov',
-        'gender'       => 'f',
-        'birthDate'    => '1972-11-05',
-        'countryName'  => 'España',
+        'gender'       => 'm',           // 'M' male, 'F' female
+        'birthDate'    => '1972-00-00',  // Valid if only the year is known
+        'countryName'  => 'España',      // In Spanish, preferably
         'arrivalDate'  => '2016-09-26',
     ],
     [
         'isSpanish'    => false,
-        'idNumber'     => '82021854627922',
-        'docType'      => 'p',
+        'idNumber'     => '82021854627922', // 14 chars. max. for Passports
+        'docType'      => 'p',              // Passport
         'docIssueDate' => '2011-11-24',
         'lastName1'    => 'Munroe',
         'lastName2'    => '',
@@ -36,19 +41,6 @@ $guests = [
         'gender'       => 'f',
         'birthDate'    => '1972-04-28',
         'countryName'  => 'Holanda',
-        'arrivalDate'  => '2016-09-26',
-    ],
-    [
-        'isSpanish'    => false,
-        'idNumber'     => '09982824072174',
-        'docType'      => 'p',
-        'docIssueDate' => '2009-09-22',
-        'lastName1'    => 'Nuremberg',
-        'lastName2'    => '',
-        'firstName'    => 'Aindriú',
-        'gender'       => 'm',
-        'birthDate'    => '1975-09-05',
-        'countryName'  => 'Reino Unido',
         'arrivalDate'  => '2016-09-26',
     ],
 ];
@@ -68,8 +60,26 @@ $hotels = [
     ]
 ];
 
-$gr = new GuestReport(new HotelFactory, new GuestFactory);
 
-$gr->setReportNumber($reportNumber)
-->setChain('ZYXWV98765', 'Simple Tiger Hotels', $reportDateTime, $hotels)
-->save();
+// Single Hotel
+
+extract($hotels[0]); // for the sake of simplicty
+
+$guestReport = new GuestReport(new HotelFactory, new GuestFactory);
+
+$guestReport
+->setReportNumber($reportNumber) // Optional
+->setHotel($hotelCode, $hotelName, $reportDateTime, $guests)
+->setDirectoryPath(__DIR__.'/out') // will try to create the directory if doesn't exist
+->save();                          // will overwrite any file with the same filename
+
+
+// Multiple Hotels (Hotel Chain)
+
+$guestReport = new GuestReport(new HotelFactory, new GuestFactory);
+
+$guestReport
+->setReportNumber($reportNumber) // Optional
+->setChain($chainCode, $chainName, $reportDateTime, $hotels)
+->setDirectoryPath(__DIR__.'/out', false) // will raise an exception if the dir. doesn't exist
+->save(false);                            // will raise an exception if the file already exists
